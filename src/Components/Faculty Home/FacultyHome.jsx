c5e2635ecdc301ce2d768b140d8c769f65b96781
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2024-04-07 00:38:42 +0530

    Commit

--- src/Components/Faculty Home/FacultyHome.jsx
+++ src/Components/Faculty Home/FacultyHome.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const FacultyHome = () => {
   return (
-    <div>FacultyHome</div>
-  )
-}
+    <div className="faculty-home">
+      <h1 className="faculty-home-heading">Faculty Dashboard</h1>
+      <div className="faculty-home-div">
+        <div className="faculty-home-buttons">
+          <button>Create Assignment</button>
+        </div>
+        <div className="faculty-home-buttons">
+          <button>Check Assignment</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FacultyHome
+export default FacultyHome;
