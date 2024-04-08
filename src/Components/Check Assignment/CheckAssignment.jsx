0379c3a5b69e5a6a7e834134ec5d346a4c94d8d9
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2024-04-09 00:21:33 +0530

    Commit

--- src/Components/Check Assignment/CheckAssignment.jsx
+++ src/Components/Check Assignment/CheckAssignment.jsx
@@ -1,16 +1,97 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { child, get, getDatabase, ref } from "firebase/database";
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { app } from "../../Firebase/firebase";
 
 const CheckAssignment = () => {
-  const facultyInfo = useSelector((state)=>state.facultyProfile);
+  const [activeAssignments, setActiveAssignments] = useState();
+  const [inactiveAssignments, setInActiveAssignments] = useState();
+  const [noActiveAssignments, setNoActiveAssignments] = useState("");
+  const [noInActiveAssignments, setNoInActiveAssignments] = useState("");
+  const facultyInfo = useSelector((state) => state.facultyProfile);
+  const navigate = useNavigate();
+  const database = getDatabase(app);
+  const fetchActiveData = useCallback(async () => {
+    await get(
+      child(
+        ref(database),
+        `${facultyInfo.semester}/${facultyInfo.stream}/${facultyInfo.subjectName}/assignments/active`
+      )
+    ).then((value) => {
+      const result = value.val();
+      if (Object.keys(result).length === 1) {
+        setNoActiveAssignments("No Active Assignments");
+      } else {
+        setActiveAssignments(result);
+      }
+    });
+  }, []);
 
+  const fetchInActiveData = useCallback(async () => {
+    await get(
+      child(
+        ref(database),
+        `${facultyInfo.semester}/${facultyInfo.stream}/${facultyInfo.subjectName}/assignments/inactive`
+      )
+    ).then((value) => {
+      const result = value.val();
+      if (Object.keys(result).length === 1) {
+        setNoInActiveAssignments("No InActive Assignments");
+      } else {
+        setInActiveAssignments(result);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchActiveData();
+    fetchInActiveData();
+  }, []);
   return (
     <div>
       <h1>All Assignments</h1>
-      <div className='left-section'></div>
-      <div className='right-section'></div>
+      <div className="left-section">
+        <div className="active-assignments">
+          <h5>Active Assignments</h5>
+          {noActiveAssignments && <p>{noActiveAssignments}</p>}
+          {activeAssignments &&
+            activeAssignments.map((key, index) => {
+              if (index === 0) {
+                return null;
+              }
+              return (
+                <div className="assignment-card" key={index}>
+                  <p>{key?.assignmentName}</p>
+                  <p>Description : {key?.assignmentDescription}</p>
+                  <p>{key?.status}</p>
+                  <p>Last Date : {key?.submissionDate}</p>
+                </div>
+              );
+            })}
+        </div>
+        <div className="inactive-assignments">
+          <h5>Closed Assignments</h5>
+        {noInActiveAssignments && <p  >{noInActiveAssignments}</p>}
+          {inactiveAssignments &&
+            inactiveAssignments.map((key, index) => {
+              if (index === 0) {
+                return null;
+              }
+              return (
+                <div className="assignment-card" key={index}>
+                  <p>{key?.assignmentName}</p>
+                  <p>Description : {key?.assignmentDescription}</p>
+                  <p>{key?.status}</p>
+                  <p>Last Date : {key?.submissionDate}</p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="right-section"></div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckAssignment
+export default CheckAssignment;
