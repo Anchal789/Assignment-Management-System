0153ad2faad445e108df9d4e1a955e68907a4914
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2024-04-13 18:04:58 +0530

    Commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,64 +2,84 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import CollegeLogo from "../../../Assets/college_logo-removebg-preview.png";
-
+import FlareIcon from '@mui/icons-material/Flare';
 export const Home = () => {
   const navigate = useNavigate();
+
   return (
     <div className="home-container">
-      <div className="home-left-section">
-        <img src={CollegeLogo} alt="college logo" className="college-logo" />
-
-        <h1 className="welcome-text">Welcome</h1>
-        <div className="home-content">
-          <div className="faculty-div">
-            <h1 className="home-headings">Faculty</h1>
-            <button
-              type="button"
-              onClick={() => {
-                navigate("/faculty signup");
-              }}
-              className="signup_btn"
-            >
-              Sign Up
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                navigate("/faculty login");
-              }}
-              className="login_btn"
-            >
-              Login
-            </button>
-          </div>
-          <div className="student-div">
-            <h1 className="home-headings">Student</h1>
-            <button
-              type="button"
-              onClick={() => {
-                navigate("/student signup");
-              }}
-              className="signup_btn"
-            >
-              Sign Up
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                navigate("/student login");
-              }}
-              className="login_btn"
-            >
-              Login
-            </button>
-          </div>
+      <div className="top-container">
+        <div className="top-overlay"></div>
+        <div className="top-content">
+          <img src={CollegeLogo} alt="college logo" className="college-logo" />
+          <h1 className="project-heading">
+            Welcome to BITS Online Assignment Management System
+          </h1>
         </div>
       </div>
-      <div className="home-right-section">
-        <h1 className="college-name">Online Assignment Management System</h1>
-        <h4 className="college-name">Online Assignment Management System</h4>
+      <div className="section-container">
+        {/* Left Section */}
+        <div className="home-left-section">
+          {/* <h2 className="section-heading">College Assignment</h2> */}
+          <div className="home-content">
+            <div className="faculty-div">
+              <h3 className="section-subheading">Faculty</h3>
+              <div className="home-button-section">
+                <button
+                  type="button"
+                  onClick={() => navigate("/faculty signup")}
+                  className="signup-btn"
+                >
+                  Sign Up
+                </button>
+                <button
+                  type="button"
+                  onClick={() => navigate("/faculty login")}
+                  className="login-btn"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+            <div className="student-div">
+              <h3 className="section-subheading">Student</h3>
+             <div className="home-button-section">
+             <button
+                type="button"
+                onClick={() => navigate("/student signup")}
+                className="signup-btn"
+              >
+                Sign Up
+              </button>
+              <button
+                type="button"
+                onClick={() => navigate("/student login")}
+                className="login-btn"
+              >
+                Login
+              </button>
+             </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="home-right-section">
+          <h2 className="keyfeature-heading">Key Features</h2>
+
+          <ul className="feature-list">
+            <li><FlareIcon/> Create and manage assignments</li>
+            <li><FlareIcon/> Close assignments before the deadline</li>
+            <li><FlareIcon/> Submit assignments as a student </li>
+            <li><FlareIcon/> Give remarks and marks to students</li>
+            <li><FlareIcon/> Check assignment history</li>
+            <li><FlareIcon/> View marks for assignments</li>
+          </ul>
+        </div>
       </div>
+      <footer>
+        <button>Contact US</button>
+      </footer>
     </div>
   );
 };
